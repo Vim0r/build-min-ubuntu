8b7ed05bd1b72db1c7e06db3b3cb0ccd731d2438
Vim0r <Vim0r@users.noreply.github.com>
2015-08-13 11:46:07 +0800

    Update usr.sh

--- xfce4_lite/usr.sh
+++ xfce4_lite/usr.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # set ramdisk
-rm -rf ~/.mozilla/firefox/'Crash Reports' ~/.cache
-ln -sT /tmp ~/.mozilla/firefox/'Crash Reports'
+rm -rf ~/.cache
 ln -sT /tmp ~/.cache
 # set path
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
