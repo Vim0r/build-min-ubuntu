ef45180577fa23ece6eef47b59096803b569f065
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 07:42:14 +0800

    Update virtualbox.sh

--- openbox/virtualbox.sh
+++ openbox/virtualbox.sh
@@ -6,22 +6,15 @@ sudo add-apt-repository ppa:satyajit-happy/themes -y
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install build-essential xorg openbox slim thunar -y
 sudo apt-get install feh lxappearance obmenu qt4-qtconfig menu gksu hal-info upower fonts-droid -y
-sudo apt-get install numix-gtk-theme -y
-sudo apt-get install gnome-icon-theme fcitx-rime xcompmgr leafpad xfce4-notifyd xfce4-terminal xfce4-indicator-plugin -y
+sudo apt-get install gnome-icon-theme numix-gtk-theme -y
+sudo apt-get install fcitx-rime xcompmgr leafpad xfce4-notifyd xfce4-terminal xfce4-indicator-plugin -y
 
-# set ramdisk
-sudo cp /etc/fstab /etc/fstab.bak
-cd /etc
-sudo echo '' >> fstab
-sudo echo 'tmpfs /tmp tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 'tmpfs /var/tmp tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 'tmpfs /var/log tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 1 > /proc/sys/vm/swappiness
 # set audio
 sudo apt-get install alsa-base alsa-utils volumeicon-alsa -y
 sudo alsactl init
 
 # set autostart
+mkdir ~/.config
 mkdir ~/.config/openbox
 cd ~/.config/openbox
 echo '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &' > autostart
@@ -29,13 +22,6 @@ echo 'eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &' >> 
 echo 'xcompmgr -cCfF -r5 -o.30 -l-5 -t-1 -D5 &' >> autostart
 echo 'eval `cat $HOME/.fehbg` &' >> autostart
 echo 'xfce4-panel &' >> autostart
-echo 'volumeicon &' >> autostart
-echo 'numlockx on &' >> autostart
-echo 'xscreensaver -no-splash &' >> autostart
-
-# set ramdisk
-rm -rf ~/.cache
-ln -sT /tmp ~/.cache
 
 # set path
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
