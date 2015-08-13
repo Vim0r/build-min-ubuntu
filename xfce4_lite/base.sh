f0001fb3c0fa485dd65972ca0d19050dc24ebe0f
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 07:49:19 +0800

    Update base.sh

--- xfce4_lite/base.sh
+++ xfce4_lite/base.sh
@@ -11,19 +11,11 @@ sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install bcmwl-kernel-source broadcom-sta-common -y
 sudo apt-get install xorg build-essential xfce4 slim xubuntu-artwork gtk2-engines-xfce gtk3-engines-xfce pixmap xfce4-indicator-plugin -y 
 sudo apt-get install wicd firefox firefox-locale-zh-hans gnome-terminal fcitx-rime gedit synaptic language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base fonts-droid qt4-qtconfig -y
-sudo apt-get purge network-manager -y
-
-# set ramdisk
-sudo cp /etc/fstab /etc/fstab.bak
-cd /etc
-sudo echo '' >> fstab
-sudo echo 'tmpfs /tmp tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 'tmpfs /var/tmp tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 'tmpfs /var/log tmpfs noatime,size=256M 0 0' >> fstab
-sudo echo 1 > /proc/sys/vm/swappiness
+#sudo apt-get purge network-manager -y
 
 # set network
-sudo cp /etc/network/interfaces /etc/network/interfaces.bak
-sudo sed -i '/usb0/d' /etc/network/interfaces
+#sudo cp /etc/network/interfaces /etc/network/interfaces.bak
+#sudo sed -i '/usb0/d' /etc/network/interfaces
 
-sudo reboot
+# set path
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
