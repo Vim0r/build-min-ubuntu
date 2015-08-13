c70432ae327c0d20db88ea3f0cc48d855c88bc13
Vim0r <Vim0r@users.noreply.github.com>
2015-08-13 12:33:43 +0800

    Update virtualbox.sh

--- openbox/virtualbox.sh
+++ openbox/virtualbox.sh
@@ -8,8 +8,8 @@ sudo apt-get install python-software-properties software-properties-common -y
 sudo add-apt-repository ppa:satyajit-happy/themes -y
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install xorg openbox slim thunar -y
-sudo apt-get install feh lxappearance obmenu qt4-qtconfig gtk-chtheme menu build-essential fonts-droid gksu hal-info upower -y
-sudo apt-get install wicd tint2 fcitx-rime xcompmgr leafpad xfce4-notifyd -y
+sudo apt-get install feh lxappearance obmenu qt4-qtconfig gtk-chtheme menu build-essential fonts-droid gksu hal-info upower gnome-icon-theme -y
+sudo apt-get install tint2 fcitx-rime xcompmgr leafpad xfce4-notifyd xfce4-terminal -y
 sudo apt-get install numix-gtk-theme -y
 
 # set ramdisk
