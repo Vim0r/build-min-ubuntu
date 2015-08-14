08acb3658f0cfd91f380962a3a7d32b61b8630b1
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 19:40:00 +0800

    Create power.sh

---
+++ openbox/power.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# add usr to power group
+sudo groupadd power
+sudo gpasswd -a nuc power
+
+# set polkit
+sudo chmod 776 /etc/polkit-1/localauthority
+sudo sed 's/auth_admin_keep/yes/' /usr/share/polkit-1/actions/org.freedesktop.consolekit.policy
