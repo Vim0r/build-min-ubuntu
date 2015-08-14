ecf4308aa04214cbe790981eb29eedfb8bb6f9e4
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 20:27:29 +0800

    Update power.sh

--- openbox/power.sh
+++ openbox/power.sh
@@ -5,5 +5,6 @@ sudo groupadd power
 sudo gpasswd -a nuc power
 
 # set polkit
-sudo chmod 776 /etc/polkit-1/localauthority
+sudo chmod 775 /etc/polkit-1/localauthority
+sudo chmod 775 /var/lib/polkit-1
 sudo sed -i 's/auth_admin_keep/yes/' /usr/share/polkit-1/actions/org.freedesktop.consolekit.policy
