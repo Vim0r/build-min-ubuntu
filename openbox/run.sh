a4e06370f138bcfebf6a2b3e7b9f6ae0a2479f9c
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 16:32:55 +0800

    Update run.sh

--- openbox/run.sh
+++ openbox/run.sh
@@ -4,12 +4,12 @@
 sudo apt-get install python-software-properties software-properties-common -y
 sudo add-apt-repository ppa:satyajit-happy/themes -y
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install bcmwl-kernel-source broadcom-sta-common -y
 sudo apt-get install build-essential xorg openbox slim thunar -y
+sudo apt-get install bcmwl-kernel-source broadcom-sta-common -y
 sudo apt-get install feh lxappearance obmenu qt4-qtconfig menu gksu hal-info upower -y
 sudo apt-get install gnome-icon-theme numix-gtk-theme -y
-sudo apt-get install fcitx-rime xcompmgr xfce4-notifyd xfce4-terminal xfce4-indicator-plugin fonts-droid -y
-sudo apt-get install wicd xfce4-screenshooter numlockx xarchiver p7zip-full rar unrar thunar-archive-plugin firefox firefox-locale-zh-hans synaptic -y
+sudo apt-get install fcitx-rime xcompmgr xfce4-notifyd xfce4-terminal xfce4-indicator-plugin fonts-droid xscreensaver -y
+sudo apt-get install wicd xfce4-screenshooter xarchiver unzip p7zip-full rar unrar thunar-archive-plugin firefox firefox-locale-zh-hans -y
 
 # set audio
 sudo apt-get install alsa-base alsa-utils -y
@@ -18,6 +18,8 @@ sudo apt-get install volumeicon-alsa -y
 
 # set path
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
+rm -rf ~/.cache
+ln -sT /tmp ~/.cache
 
 # set autostart
 mkdir ~/.config/openbox
@@ -29,7 +31,9 @@ echo 'xcompmgr -cCfF -r5 -o.30 -l-5 -t-1 -D5 &' >> autostart
 echo 'xscreensaver -no-splash &' >> autostart
 echo 'xfce4-panel &' >> autostart
 echo 'volumeicon &' >> autostart
-echo 'numlockx on &' >> autostart
+
+# set network
+sudo sed '/usb/d' /etc/network/interfaces
 
 # set terminal
 # =======add in /home/vm/.config/openbox/rc.xml==========
