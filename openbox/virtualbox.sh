69b290e8ea27b9b8dfd6d201210da3f054ef0c32
Vim0r <Vim0r@users.noreply.github.com>
2015-08-14 17:58:44 +0800

    Update virtualbox.sh

--- openbox/virtualbox.sh
+++ openbox/virtualbox.sh
@@ -10,16 +10,15 @@ sudo apt-get install gnome-icon-theme numix-gtk-theme -y
 sudo apt-get install fcitx-rime xcompmgr leafpad xfce4-notifyd xfce4-terminal xfce4-indicator-plugin -y
 
 # set audio
-sudo apt-get install alsa-base alsa-utils volumeicon-alsa -y
+sudo apt-get install alsa-base alsa-utils -y
 sudo alsactl init
 
 # set autostart
-sudo cd /etc/xdg/openbox/
-sudo echo '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &' > autostart
-sudo echo 'eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &' >> autostart
-sudo echo 'eval `cat $HOME/.fehbg` &' >> autostart
-sudo echo 'xcompmgr -cCfF -r5 -o.30 -l-5 -t-1 -D5 &' >> autostart
-sudo echo 'xfce4-panel &' >> autostart
+mkdir ~/.config/openbox
+cd ~/.config/openbox
+echo 'xcompmgr -cCfF -r5 -o.30 -l-5 -t-1 -D5 &' > autostart
+echo 'eval `cat $HOME/.fehbg` &' >> autostart
+echo 'xfce4-panel &' >> autostart
 
 # set path
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
