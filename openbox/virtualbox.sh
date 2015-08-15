0126e6e13790abadec52a90a809266e5ec9d2d23
Vim0r <Vim0r@users.noreply.github.com>
2015-08-15 09:53:56 +0800

    Update virtualbox.sh

--- openbox/virtualbox.sh
+++ openbox/virtualbox.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # install program
-sudo apt-get install python-software-properties software-properties-common -y
-sudo add-apt-repository ppa:satyajit-happy/themes -y
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install build-essential xorg openbox slim thunar -y
-sudo apt-get install feh lxappearance obmenu qt4-qtconfig menu gksu hal-info upower fonts-droid -y
-sudo apt-get install gnome-icon-theme numix-gtk-theme -y
-sudo apt-get install fcitx-rime xcompmgr leafpad xfce4-notifyd xfce4-terminal xfce4-indicator-plugin -y
+sudo apt-get install build-essential xorg openbox thunar -y
+sudo apt-get install gksu hal-info upower pm-utils python-software-properties software-properties-common -y
+sudo apt-get install gnome-icon-theme feh lxappearance obmenu menu -y
+sudo apt-get install fcitx-rime fonts-droid tint2 -y
+#sudo apt-get install xfce4-notifyd xfce4-terminal xfce4-indicator-plugin -y
 
 # set audio
 sudo apt-get install alsa-base alsa-utils -y
@@ -16,9 +15,7 @@ sudo alsactl init
 # set autostart
 mkdir ~/.config/openbox
 cd ~/.config/openbox
-echo 'xcompmgr -cCfF -r5 -o.30 -l-5 -t-1 -D5 &' > autostart
-echo 'eval `cat $HOME/.fehbg` &' >> autostart
-echo 'xfce4-panel &' >> autostart
+echo 'eval `cat $HOME/.fehbg` &' > autostart
 
 # set path
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:/sbin:/bin:/usr/game:$PATH
