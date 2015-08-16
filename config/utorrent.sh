dd14c124177a63685da17a5013595d1467085484
Vim0r <Vim0r@users.noreply.github.com>
2015-08-16 17:13:42 +0800

    Update utorrent.sh

--- config/utorrent.sh
+++ config/utorrent.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# ======How to use===========
-# download utorrent-server from https://www.utorrent.com/intl/zh_cn/downloads/linux
+# ======How to install===========
+### download utorrent-server from https://www.utorrent.com/intl/zh_cn/downloads/linux
 # cd ~/download
 # tar -xzf utorrent*.tar.gz
 # sudo chmod 776 utorrent*/*
 # mv utorrent* .utserver
 # mv .utserver ~/.utserver
 # nano utorrent.sh
-# cope ALL TEXT to the file
+### cope ALL TEXT to the file
 # chmod 777 ~/.utserver/utorrent.sh
+# ======How to use===========
+# sh ~/.utserver/utorrent.sh
+### open url=http://localhost:8080/gui/web/index.html
+### default usrname=admin
+### no passwd
 
 cd ~/.utserver
 ./utserver &
