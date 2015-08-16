46f45ab3b69e9f8fecc8d7d3e8a77c2cc7a03547
Vim0r <Vim0r@users.noreply.github.com>
2015-08-16 09:52:58 +0800

    Update sys.sh

--- openbox/sys.sh
+++ openbox/sys.sh
@@ -4,9 +4,10 @@
 sudo apt-get install build-essential xorg openbox thunar -y
 sudo apt-get install bcmwl-kernel-source broadcom-sta-common -y
 sudo apt-get install gksu hal-info upower pm-utils python-software-properties software-properties-common -y
-sudo apt-get install feh tint2 lxappearance obmenu qt4-qtconfig menu gnome-icon-theme ubuntu-mono pixmap gtk2-engines-murrine gtk2-engines-pixbuf gtk3-engines-unico dmz-cursor-theme -y
+sudo apt-get install feh tint2 lxappearance obmenu qt4-qtconfig gnome-icon-theme ubuntu-mono pixmap gtk2-engines-murrine gtk2-engines-pixbuf gtk3-engines-unico dmz-cursor-theme -y
 sudo apt-get install fcitx-rime wicd compton fonts-droid xscreensaver numlockx -y
-sudo apt-get install xarchiver unzip p7zip-full rar unrar firefox firefox-locale-zh-hans -y
+sudo apt-get install xarchiver zip unzip p7zip-full rar unrar firefox firefox-locale-zh-hans -y
+#sudo apt-get install openbox-xdgmenu xdotool thunar-archive-plugin -y
 
 # set audio
 sudo apt-get install alsa-base alsa-utils alsa-tools-gui -y
@@ -24,10 +25,9 @@ mkdir ~/.config/openbox
 cd ~/.config/openbox
 echo 'xscreensaver -no-splash &' > autostart
 echo 'eval `cat $HOME/.fehbg` &' >> autostart
-echo 'compton -cC -f -i 1.0 -e 1.0 -o 1.0 -r 6 -l -4 -t -4 -I 0.30 -O 0.05 -D 2 -G -b &' >> autostart
-echo 'numlockx &' >> autostart
-echo '(sleep 2s && tint2) &' >> autostart
-echo '(sleep 3s && volumeicon) &' >> autostart
+echo 'compton -cb -I 0.30 --shadow-opacity 0.7 --shadow-offset-x -11 --shadow-offset-y -11' >> autostart
+echo '(sleep 1s && tint2) &' >> autostart
+echo '(sleep 2s && volumeicon) &' >> autostart
 
 # set network
 mkdir ~/bak
@@ -36,10 +36,8 @@ sudo sed -i '/usb/d' /etc/network/interfaces
 
 # startx openbox-session
 cd ~
-
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo 'exec openbox-session' >> .xinitrc
-
 cp /etc/profile ~/.profile
 echo '
 if [ -z "$DISPLAY" ] && [ $(tty) == /dev/tty1 ]; then
