e6dbed6c7575067911d649b9b873c70782601ccd
Vim0r <Vim0r@users.noreply.github.com>
2015-08-16 12:16:49 +0800

    Update run.sh

--- xfce4_lite/run.sh
+++ xfce4_lite/run.sh
@@ -9,8 +9,8 @@ sudo apt-add-repository ppa:xubuntu-dev/xfce-4.12 -y
 sudo apt-get update && sudo apt-get dist-upgrade -y
 # install wlan driver
 sudo apt-get install bcmwl-kernel-source broadcom-sta-common -y
-sudo apt-get install xorg build-essential xfce4 slim xubuntu-artwork gtk2-engines-xfce gtk3-engines-xfce pixmap xfce4-indicator-plugin -y 
-sudo apt-get install wicd firefox firefox-locale-zh-hans gnome-terminal fcitx-rime gedit synaptic language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base fonts-droid qt4-qtconfig -y
+sudo apt-get install xorg build-essential xfce4 slim xubuntu-artwork gnome-icon-theme ubuntu-mono pixmap gtk2-engines-murrine gtk2-engines-pixbuf gtk3-engines-unico xfce4-indicator-plugin -y 
+sudo apt-get install wicd firefox firefox-locale-zh-hans xfce4-terminal fcitx-rime language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base fonts-droid qt4-qtconfig -y
 #sudo apt-get purge network-manager -y
 
 # set network
