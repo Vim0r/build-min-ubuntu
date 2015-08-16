0c9def14b07be0c206f17ada1a826a81cc4cdae4
Vim0r <Vim0r@users.noreply.github.com>
2015-08-16 17:18:57 +0800

    Update utorrent.sh

--- config/utorrent.sh
+++ config/utorrent.sh
@@ -10,6 +10,15 @@
 # nano utorrent.sh
 ### cope ALL TEXT to the file
 # chmod 777 ~/.utserver/utorrent.sh
+# nano ~/.utserver/utserver.conf
+###----cope text below without # -------
+# ut_webui_port 8080
+# dir_active ~
+# dir_torrent_files ~/btfiles/
+# bind_port 37455
+# token_auth_disable
+# ---------end copy to utserver.conf------
+
 # ======How to use===========
 # sh ~/.utserver/utorrent.sh
 ### open url=http://localhost:8080/gui/web/index.html
