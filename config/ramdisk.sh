58891a8a8184bf26a7f1c0fc69582aabe75bebdf
Vim0r <Vim0r@users.noreply.github.com>
2015-08-19 20:11:20 +0800

    Update ramdisk.sh

--- config/ramdisk.sh
+++ config/ramdisk.sh
@@ -4,4 +4,4 @@
 cp /etc/fstab ~/fstab.bak
 #sudo echo 'tmpfs /tmp tmpfs noatime,size=256M 0 0' >> /etc/fstab
 #sudo echo 'tmpfs /var/tmp tmpfs noatime,size=256M 0 0' >> /etc/fstab
-sudo echo 'tmpfs /home/usrname/.cache tmpfs noatime,size=256M 0 0' >> /etc/fstab
+#sudo echo 'tmpfs /home/usrname/.cache tmpfs noatime,size=256M 0 0' >> /etc/fstab
